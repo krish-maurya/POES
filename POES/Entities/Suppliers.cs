eee900ccc8f3ce216c9d6ccb923f8661100ace7b
Sneh Patel <snehpatel@Snehs-MacBook-Air.local>
2026-06-25 13:48:25 +0530

    supplier entity

--- POES/Entities/Suppliers.cs
+++ POES/Entities/Suppliers.cs
@@ -2,6 +2,12 @@ namespace POES.Entities;
 
 public class Suppliers
 {
-    public int SupplierCode { get; set; }
-    public required String Supplier { get; set; }
+    public required string Supplier { get; set; }      
+    public required string Description { get; set; }   
+    public required string Address { get; set; }       
+    public required string ZipCode { get; set; }       
+    public required string Town { get; set; }          
+    public required string Country { get; set; }       
+    public required string Phone { get; set; }         
+    public string? Fax { get; set; }                   
 }
