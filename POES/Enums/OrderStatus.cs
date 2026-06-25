3d0eb862c713940e8f79c5cd39dc1e2f24048e3d
krish-maurya <mauryakrish734@gmail.com>
2026-06-25 12:44:39 +0530

    Local changes

---
+++ POES/Enums/OrderStatus.cs
@@ -0,0 +1,3 @@
+namespace POES.Enums;
+
+public enum OrderStatus { OrderEntry = 1, OrderDelivery = 2, Delivered = 3 }
---
+++ POES/Enums/PackingType.cs
@@ -0,0 +1,3 @@
+namespace POES.Enums;
+
+public enum PackingType { No = 0, Yes = 1 }
