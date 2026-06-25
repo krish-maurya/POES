3206496173eee09ef593c015869a7a9895677e6a
Sneh Patel <snehpatel@Snehs-MacBook-Air.local>
2026-06-25 12:25:16 +0530

    folders created

--- POES/obj/Debug/net10.0/POES.AssemblyInfo.cs
+++ POES/obj/Debug/net10.0/POES.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POES")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f84a7314e98c7372ebc33c2d561222c112ebf7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("POES")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POES")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
